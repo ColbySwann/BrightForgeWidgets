bbe96ce46cde3a36a2dc6b92ba9ef08d988b73c9
Colby <itchellma1@gmx.com>
2025-11-13 15:27:28 -0600

    remove old widgets

---
+++ src/main/resources/db/migration/V20251113152228__Delete_Old_Widgets.sql
@@ -0,0 +1,9 @@
+
+DELETE FROM product
+WHERE slug IN (
+               'flux-capacitor-mk-ii',
+               'quantum-gyro-stabilizer',
+               'nanospark-emitter',
+               'photon-relay-core',
+               'cryolock-containment-seal'
+    );
